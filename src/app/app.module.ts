9af601499c9c83e66798804bad422f18f8bbd009
Dimitri Keller <dkeller@bluesolution.de>
2021-06-28 09:51:58 +0200

    add devextreme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {
+  DxSelectBoxModule, DxListModule, DxCheckBoxModule,
+  DxNumberBoxModule, DxFormModule, DxTextAreaModule,
+  DxDateBoxModule, DxButtonModule, DxTextBoxModule,
+  DxMultiViewModule, DxCalendarModule, DxTemplateModule,
+  DxLoadPanelModule, DxPopupModule, DxSwitchModule,
+  DxDataGridModule, DxBoxModule, DxRadioGroupModule,
+  DxScrollViewModule, DxGalleryModule, DxValidatorModule,
+  DxTooltipModule, DxSpeedDialActionModule, DxAccordionModule,
+  DxTabPanelModule
+} from 'devextreme-angular';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SideNavOuterToolbarModule, SideNavInnerToolbarModule, SingleCardModule } from './layouts';
+import { FooterModule, ResetPasswordFormModule, CreateAccountFormModule, ChangePasswordFormModule, LoginFormModule } from './shared/components';
+import { AuthService, ScreenService, AppInfoService } from './shared/services';
+import { UnauthenticatedContentModule } from './unauthenticated-content';
 
 @NgModule({
   declarations: [
@@ -10,9 +26,27 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SideNavOuterToolbarModule,
+    SideNavInnerToolbarModule,
+    SingleCardModule,
+    FooterModule,
+    ResetPasswordFormModule,
+    CreateAccountFormModule,
+    ChangePasswordFormModule,
+    LoginFormModule,
+    UnauthenticatedContentModule,
+    DxSelectBoxModule, DxListModule, DxCheckBoxModule,
+  DxNumberBoxModule, DxFormModule, DxTextAreaModule,
+  DxDateBoxModule, DxButtonModule, DxTextBoxModule,
+  DxMultiViewModule, DxCalendarModule, DxTemplateModule,
+  DxLoadPanelModule, DxPopupModule, DxSwitchModule,
+  DxDataGridModule, DxBoxModule, DxRadioGroupModule,
+  DxScrollViewModule, DxGalleryModule, DxValidatorModule,
+  DxTooltipModule, DxSpeedDialActionModule, DxAccordionModule,
+  DxTabPanelModule
   ],
-  providers: [],
+  providers: [AuthService, ScreenService, AppInfoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
