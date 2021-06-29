5c1a3cfa0c35d68fe558216c0c8f79235ddc5ea3
Dimitri Keller <dkeller@bluesolution.de>
2021-06-29 11:17:14 +0200

    update to angular 12

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import themes from 'devextreme/ui/themes';
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+themes.initialized(() => {
+  platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.error(err));
+});
